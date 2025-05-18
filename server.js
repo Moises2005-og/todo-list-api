ea93a0793f22ba55ef84cc4ec8a098c4db7c6d6c
moises <moisessebastiao2005@gmail.com>
2025-05-18 09:39:08 +0100

    created api

---
+++ server.js
@@ -0,0 +1,65 @@
+import express from "express"
+import {PrismaClient} from "@prisma/client"
+
+const app = express()
+app.use(express.json())
+const PORT = 3000
+const prisma = new PrismaClient()
+
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await prisma.todo.findMany()
+    
+        res.status(200).json(tasks)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
+app.post("/task", async (req, res) => {
+    try {
+        await prisma.todo.create({
+            data: {
+                task: req.body.task,
+            }
+        })
+        res.status(201).json(req.body)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
+app.delete("/task/:id", async (req, res) => {
+    try {
+        const deleteTask = await prisma.todo.delete({
+            where: {
+                id: req.params.id
+            },
+        }) 
+        res.status(200).json(deleteTask)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
+app.put("/task/:id", async(req, res) => {
+    try {
+        await prisma.todo.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                task: req.body.task
+            }
+        })
+        res.status(200).json(req.body)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
+app.listen(PORT)
